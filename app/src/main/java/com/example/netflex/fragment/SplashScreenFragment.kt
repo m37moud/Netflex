3d96f6f1bef0b15c711700b4ca7803503d3082d1
andguladzeshio <shio.andghuladze.1@btu.edu.ge>
2021-10-15 12:19:43 +0400

    nav graph and fragments with base are ready

--- app/src/main/java/com/example/netflex/fragment/SplashScreenFragment.kt
+++ app/src/main/java/com/example/netflex/fragment/SplashScreenFragment.kt
@@ -8,13 +8,13 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.netflex.R
 
-class SplashScreenFragment : Fragment() {
+class SplashScreenFragment : Fragment(R.layout.fragment_splash_screen) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        Handler(Looper.getMainLooper()).postDelayed({
-//            findNavController().navigate(R.id.action_splashScreenFragment_to_movieCollectionFragment)
-//        }, 3000)
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splashScreenFragment_to_movieCollectionFragment)
+        }, 3000)
 
     }
 }
